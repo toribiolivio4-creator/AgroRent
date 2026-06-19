049af891ba7cdcfbfec929a354662c7c6df0e187
Livio <toribiolivio4@gmail.com>
2026-06-19 09:17:27 -0300

    fix: borrado de carpetas redundantes

--- agrorent-fixed/app/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .usuario import UsuarioCreate, UsuarioOut, Token, TokenData
-from .lote import LoteCreate, LoteUpdate, LoteOut
-from .gasto import GastoCreate, GastoUpdate, GastoOut
-from .ingreso import IngresoCreate, IngresoUpdate, IngresoOut
-from .rentabilidad import RentabilidadLote, DashboardOut
